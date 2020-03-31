680dfdc03dfbe953dedef5191d620a9e0e5ced81
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-31 14:21:51 -0700

    added html placeholders and ids for javascript functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 // ======================================
 
-  // // Your web app's Firebase configuration
+  // Your web app's Firebase configuration
   // var firebaseConfig = {
   //   apiKey: "AIzaSyAXwZwmrZkcGxq5-pXZTPg--bw6bRGB2G0",
   //   authDomain: "food-share-ad623.firebaseapp.com",
@@ -23,8 +23,11 @@
   // var data = firebase.database();
   // console.log(data);
 
-
-
+$("#search-button").on("click", function(event){
+  event.preventDefault();
+  var zipcode = $("#zipcode").val().trim();
+  console.log("The zip code is:" + zipcode);
+})
 
 var foodpantries = [{name: "APA Family", address: "50 RAYMOND (near San Bruno in Visitation Valley).", hours: "Fri 11am-3pm"},{name: "Bayview Opera House", address: "4705 THIRD STREET (at Newcomb in the Bayview)", hours: "Mon 10am-2pm"}, {name: "Bessie Carmichael", address: "375 7TH ST. (near Harrison in the SOMA District)", hours: "Th 10am-1pm"}, {name:"Cesar Chavez Elementary", address:"825 SHOTWELL (bet 22 & 23rd Sts in the Mission)", hours: "Tu 10am-1pm"},{name: "Francisco Middle School", address:"2190 POWELL (bet. Francisco and Chestnut in North Beach)", hours: "Th 10am-1pm"}, {name:"James Denman Middle School", address:"241 ONEIDA (at Otsego in the Excelsior)", hours:"Wed 10am-1pm"}, {name: "Lincoln High School", address:"2162 24TH AVE (bet Rivera and Quintara in the Sunset)", hours: "Fri 10am-1pm"}]
 
