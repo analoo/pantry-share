38ad5ae20f37bfe2259e1061d4174bae9c229711
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-02 11:27:01 -0700

    fixed map styling and data reference for display pantry().

--- js/script.js
+++ js/script.js
@@ -16,147 +16,146 @@ function displayFoodPantries() {
   var header = $("<h3>Find Food Here</h3>");
   $("#food-options").append(header);
 
-  for (let i = 0; i < foodAddresses.length; i++) {
-    if (foodAddresses[i].zipcode === userzip) {
+  for (let i = 0; i < foodpantries.length; i++) {
+    if (foodpantries[i].zipcode === userzip) {
       var div = $("<div class='pantry'></div>")
       $("#food-options").append(div);
-      $(div).append("<p>" + foodAddresses[i].name + "</p>")
-      $(div).append("<p>" + foodAddresses[i].address + "</p>")
-      $(div).append("<p>" + foodAddresses[i].hours + "</p>")
+      $(div).append("<p>" + foodpantries[i].name + "</p>")
+      $(div).append("<p>" + foodpantries[i].address + "</p>")
+      $(div).append("<p>" + foodpantries[i].hours + "</p>")
     }
     else if (userzip === "") {
       var div = $("<div class='pantry'></div>")
       $("#food-options").append(div);
-      $(div).append("<p>" + foodAddresses[i].name + "</p>")
-      $(div).append("<p>" + foodAddresses[i].address + "</p>")
-      $(div).append("<p>" + foodAddresses[i].hours + "</p>")
+      $(div).append("<p>" + foodpantries[i].name + "</p>")
+      $(div).append("<p>" + foodpantries[i].address + "</p>")
+      $(div).append("<p>" + foodpantries[i].hours + "</p>")
     }
   }
 
-}
+  var californiaData = values.food.CA;
+  if (californiaData) {
+    var objID = Object.keys(californiaData);
 
-var californiaData = values.food.CA;
-if (californiaData) {
-  var objID = Object.keys(californiaData);
+    for (let i = 0; i < objID.length; i++) {
+      if (californiaData[objID[i]].zipcode === userzip && californiaData[objID[i]].claimed === false) {
+        var div = $("<div class='user'></div>");
+        $("#food-options").append(div);
+        $(div).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
+        $(div).append("<p>" + californiaData[objID[i]].name + "</p>")
+        $(div).append("<p>" + californiaData[objID[i]].description + "</p>")
+        $(div).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
 
-  for (let i = 0; i < objID.length; i++) {
-    if (californiaData[objID[i]].zipcode === userzip && californiaData[objID[i]].claimed === false) {
-      var div = $("<div class='user'></div>");
-      $("#food-options").append(div);
-      $(div).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
-      $(div).append("<p>" + californiaData[objID[i]].name + "</p>")
-      $(div).append("<p>" + californiaData[objID[i]].description + "</p>")
-      $(div).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
+        var user = {
+          "name": californiaData[objID[i]].name,
+          "description": californiaData[objID[i]].description,
+          "zipcode": californiaData[objID[i]].zipcode
+        }
 
-      var user = {
-        "name": californiaData[objID[i]].name,
-        "description": californiaData[objID[i]].description,
-        "zipcode": californiaData[objID[i]].zipcode
+        userSubmissions.push(user)
       }
 
-      userSubmissions.push(user)
-    }
+      else if (userzip === "" && californiaData[objID[i]].claimed === false) {
+        var div = $("<div class='user'></div>");
+        $("#food-options").append(div);
+        $(div).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
+        $(div).append("<p>" + californiaData[objID[i]].name + "</p>")
+        $(div).append("<p>" + californiaData[objID[i]].description + "</p>")
+        $(div).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
 
-    else if (userzip === "" && californiaData[objID[i]].claimed === false) {
-      var div = $("<div class='user'></div>");
-      $("#food-options").append(div);
-      $(div).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
-      $(div).append("<p>" + californiaData[objID[i]].name + "</p>")
-      $(div).append("<p>" + californiaData[objID[i]].description + "</p>")
-      $(div).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
+        var user = {
+          "name": californiaData[objID[i]].name,
+          "description": californiaData[objID[i]].description,
+          "zipcode": californiaData[objID[i]].zipcode
+        }
 
-      var user = {
-        "name": californiaData[objID[i]].name,
-        "description": californiaData[objID[i]].description,
-        "zipcode": californiaData[objID[i]].zipcode
+        userSubmissions.push(user)
       }
-
-      userSubmissions.push(user)
     }
   }
 }
 
-$("#search-button").on("click", function (event) {
-  event.preventDefault();
-  userzip = $("#zipcode").val().trim();
-  displayFoodPantries();
-})
+  $("#search-button").on("click", function (event) {
+    event.preventDefault();
+    userzip = $("#zipcode").val().trim();
+    displayFoodPantries();
+  })
 
 
-// ======= firebase configuration ==========
+  // ======= firebase configuration ==========
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAXwZwmrZkcGxq5-pXZTPg--bw6bRGB2G0",
-  authDomain: "food-share-ad623.firebaseapp.com",
-  databaseURL: "https://food-share-ad623.firebaseio.com",
-  projectId: "food-share-ad623",
-  storageBucket: "food-share-ad623.appspot.com",
-  messagingSenderId: "999281031297",
-  appId: "1:999281031297:web:f9d265166ebfc345763580",
-  measurementId: "G-3EYCVELK5L"
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyAXwZwmrZkcGxq5-pXZTPg--bw6bRGB2G0",
+    authDomain: "food-share-ad623.firebaseapp.com",
+    databaseURL: "https://food-share-ad623.firebaseio.com",
+    projectId: "food-share-ad623",
+    storageBucket: "food-share-ad623.appspot.com",
+    messagingSenderId: "999281031297",
+    appId: "1:999281031297:web:f9d265166ebfc345763580",
+    measurementId: "G-3EYCVELK5L"
+  };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-var database = firebase.database();
+  var database = firebase.database();
 
-var name = "";
-var address = "";
-var zipcode = "";
-var description = "";
-var claimed = "";
-var hours = "";
+  var name = "";
+  var address = "";
+  var zipcode = "";
+  var description = "";
+  var claimed = "";
+  var hours = "";
 
-$("#post-food-button").on("click", function (event) {
-  event.preventDefault();
-  name = $("#food-address").val().trim();
-  if (name === "") {
-    name = "anonymous neighbor";
-  }
-  address = $("#food-address").val().trim();
-  zipcode = $("#food-zip").val().trim();
-  description = $("#food-desc").val().trim();
-  claimed = false;
-  state = "CA";
+  $("#post-food-button").on("click", function (event) {
+    event.preventDefault();
+    name = $("#food-address").val().trim();
+    if (name === "") {
+      name = "anonymous neighbor";
+    }
+    address = $("#food-address").val().trim();
+    zipcode = $("#food-zip").val().trim();
+    description = $("#food-desc").val().trim();
+    claimed = false;
+    state = "CA";
 
-  // date stamp time
-  hours = "not applicable";
-  writeUserData(name, state, address, zipcode, description, claimed, hours);
-  $("form").css("display", "none")
+    // date stamp time
+    hours = "not applicable";
+    writeUserData(name, state, address, zipcode, description, claimed, hours);
+    $("form").css("display", "none")
 
-});
-
-// saves data to the firebase
-var pathStr = "";
-
-function writeUserData(name, state, address, zipcode, description, claimed, hours) {
-  database.ref("resources/food/" + state).push({
-    name: name,
-    address: address,
-    zipcode: zipcode,
-    description: description,
-    claimed: claimed,
-    hours: hours
-  }
-  );
-}
-
-var values;
-database.ref("resources").on("value", function (snapshot) {
-  values = snapshot.val();
-  displayFoodPantries();
-})
-
-
-$(document).on("click", ".dibs", function (event) {
-  event.preventDefault();
-  var dataKey = $(this).attr("data");
-  var claimedRef = database.ref("resources/food/CA/" + dataKey);
-
-  claimedRef.update({
-    "claimed": "true"
   });
-  displayFoodPantries();
 
-})
+  // saves data to the firebase
+  var pathStr = "";
+
+  function writeUserData(name, state, address, zipcode, description, claimed, hours) {
+    database.ref("resources/food/" + state).push({
+      name: name,
+      address: address,
+      zipcode: zipcode,
+      description: description,
+      claimed: claimed,
+      hours: hours
+    }
+    );
+  }
+
+  var values;
+  database.ref("resources").on("value", function (snapshot) {
+    values = snapshot.val();
+    displayFoodPantries();
+  })
+
+
+  $(document).on("click", ".dibs", function (event) {
+    event.preventDefault();
+    var dataKey = $(this).attr("data");
+    var claimedRef = database.ref("resources/food/CA/" + dataKey);
+
+    claimedRef.update({
+      "claimed": "true"
+    });
+    displayFoodPantries();
+
+  })
