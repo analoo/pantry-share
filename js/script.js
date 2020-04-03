c046785d44b4b68042be453877c332ba2a14d4eb
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-02 17:27:34 -0700

    added input validator on zip code to prevent user from submitting data if zip code is not numeric or less than 5 in length

--- js/script.js
+++ js/script.js
@@ -1,99 +1,23 @@
-// Stephon place for code
-
-
-
-
-// ======================================
-
-// Your web app's Firebase configuration
-
-//=====Find food tab==============
-
 var userzip = "";
-
-function displayFoodPantries() {
-  $("#food-options").empty();
-  var table = $("<table>Find Food Here</table>");
-  $("#food-options").append(table);
-  var tableHeader = $("<tr><th>Food Pantries</th></tr>");
-  $(table).append(tableHeader);
-
-  for (let i = 0; i < foodpantries.length; i++) {
-    if (foodpantries[i].zipcode === userzip) {
-      var trow = $("<tr class='pantry'></tr>")
-      var div = $("<td></td>")
-      $(table).append(trow);
-      $(trow).append(div);
-      $(div).append("<p>" + foodpantries[i].name + "</p>")
-      $(div).append("<p>" + foodpantries[i].address + "</p>")
-      $(div).append("<p>" + foodpantries[i].hours + "</p>")
-    }
-    else if (userzip === "") {
-      var trow = $("<tr class='pantry'></tr>")
-      var div = $("<td></td>")
-      $(table).append(trow);
-      $(trow).append(div);
-      $(div).append("<p>" + foodpantries[i].name + "</p>")
-      $(div).append("<p>" + foodpantries[i].address + "</p>")
-      $(div).append("<p>" + foodpantries[i].hours + "</p>")
-    }
-  }
-
-  var californiaData = values.food.CA;
-  if (californiaData) {
-    var objID = Object.keys(californiaData);
-    var table2 = $("<table></table>");
-    $("#food-options").append(table2);
-    var tableHeader2 = $("<tr><th>User Generated</th></tr>");
-    $(table2).append(tableHeader2);
-
-    for (let i = 0; i < objID.length; i++) {
-      if (californiaData[objID[i]].zipcode === userzip && californiaData[objID[i]].claimed === false) {
-        var trow2 = $("<tr class='users'></tr>")
-        var div2 = $("<td></td>")
-        $(table2).append(trow2);
-        $(trow2).append(div2);
-        $(div2).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
-        $(div2).append("<p>" + californiaData[objID[i]].name + "</p>")
-        $(div2).append("<p>" + californiaData[objID[i]].description + "</p>")
-        $(div2).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
-
-        var user = {
-          "name": californiaData[objID[i]].name,
-          "description": californiaData[objID[i]].description,
-          "zipcode": californiaData[objID[i]].zipcode
-        }
-
-        userSubmissions.push(user)
-      }
-
-      else if (userzip === "" && californiaData[objID[i]].claimed === false) {
-        var trow2 = $("<tr class='users'></tr>")
-        var div2 = $("<td></td>")
-        $(table2).append(trow2);
-        $(trow2).append(div2);
-        $(div2).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
-        $(div2).append("<p>" + californiaData[objID[i]].name + "</p>")
-        $(div2).append("<p>" + californiaData[objID[i]].description + "</p>")
-        $(div2).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
-
-        var user = {
-          "name": californiaData[objID[i]].name,
-          "description": californiaData[objID[i]].description,
-          "zipcode": californiaData[objID[i]].zipcode
-        }
-
-        userSubmissions.push(user)
-      }
-    }
-  }
-}
 
 $("#search-button").on("click", function (event) {
   event.preventDefault();
   userzip = $("#zipcode").val().trim();
   displayFoodPantries();
 })
+
+function checkZipcodeNum(str) {
+  var allInts = true;
+  var array = str.split("");
+  var nums = "1234567890"
+  for (let i = 0; i < array.length; i++) {
+    if (nums.indexOf(array[i]) == -1) {
+      allInts = false;
+    }
+
+  }
+  return allInts
+}
 
 
 // ======= firebase configuration ==========
@@ -123,20 +47,27 @@ var hours = "";
 
 $("#post-food-button").on("click", function (event) {
   event.preventDefault();
+  zipcode = $("#food-zip").val().trim();
   name = $("#food-address").val().trim();
   if (name === "") {
     name = "anonymous neighbor";
   }
   address = $("#food-address").val().trim();
-  zipcode = $("#food-zip").val().trim();
   description = $("#food-desc").val().trim();
   claimed = false;
   state = "CA";
-
   // date stamp time
   hours = "not applicable";
-  writeUserData(name, state, address, zipcode, description, claimed, hours);
-  $("form").css("display", "none")
+
+  if (checkZipcodeNum(zipcode) == false || zipcode.length != 5) {
+    $("#zip-warning").text("zipcode is invalid. please update and resubmit form");
+  }
+
+  else {
+    $("#warning").empty()
+    writeUserData(name, state, address, zipcode, description, claimed, hours);
+    $("form").css("display", "none")
+  }
 
 });
 
@@ -173,3 +104,105 @@ $(document).on("click", ".dibs", function (event) {
   displayFoodPantries();
 
 })
+
+function displayFoodPantries() {
+  $("#food-options").empty();
+
+  // var table = $("<table>Find Food Here</table>");
+  // $("#food-options").append(table);
+  // var tableHeader = $("<tr><th>Food Pantries</th></tr>");
+  // $(table).append(tableHeader);
+
+  // for (let i = 0; i < foodpantries.length; i++) {
+  //   if (foodpantries[i].zipcode === userzip) {
+  //     var trow = $("<tr class='pantry'></tr>")
+  //     var div = $("<td></td>")
+  //     $(table).append(trow);
+  //     $(trow).append(div);
+  //     $(div).append("<p>" + foodpantries[i].name + "</p>")
+  //     $(div).append("<p>" + foodpantries[i].address + "</p>")
+  //     $(div).append("<p>" + foodpantries[i].hours + "</p>")
+  //   }
+  //   else if (userzip === "") {
+  //     var trow = $("<tr class='pantry'></tr>")
+  //     var div = $("<td></td>")
+  //     $(table).append(trow);
+  //     $(trow).append(div);
+  //     $(div).append("<p>" + foodpantries[i].name + "</p>")
+  //     $(div).append("<p>" + foodpantries[i].address + "</p>")
+  //     $(div).append("<p>" + foodpantries[i].hours + "</p>")
+  //   }
+  // }
+
+
+  // ----- start of other ---
+  // var californiaData = values.food.CA;
+  // if (californiaData) {
+  //   var objID = Object.keys(californiaData);
+  //   console.log(objID);
+  //   var table2 = $("<table></table>");
+  //   $("#food-options").append(table2);
+  //   var tableHeader2 = $("<tr><th>User Generated</th></tr>");
+  //   $(table2).append(tableHeader2);
+
+
+  //   for (let i = 0; i < objID.length; i++) {
+  //     if (californiaData[objID[i]].zipcode == userzip && californiaData[objID[i]].claimed == false) {
+  //       // creates a row for each data element and appends to the table
+  //       var trow2 = $("<tr class='users'></tr>")
+  //       $(table2).append(trow2);
+
+  //       // creates the data element and appends to row
+  //       var div2 = $("<td></td>")
+  //       $(trow2).append(div2);
+
+  //       $(div2).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].name + "</p>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].address + "</p>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].description + "</p>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
+
+  //       var user = {
+  //         "name": californiaData[objID[i]].name,
+  //         "description": californiaData[objID[i]].description,
+  //         "address": californiaData[objID[i]].address,
+  //         "zipcode": californiaData[objID[i]].zipcode
+  //       }
+
+  //       console.log(user)
+
+  //       userSubmissions.push(user)
+
+  //       console.log(userSubmissions)
+  //     }
+
+  //     else if (userzip == "" && californiaData[objID[i]].claimed == false) {
+  //       // creates a row for each data element and appends to the table
+  //       var trow2 = $("<tr class='users'></tr>")
+  //       $(table2).append(trow2);
+
+  //       // creates the data element and appends to row
+  //       var div2 = $("<td></td>")
+  //       $(trow2).append(div2);
+  //       $(div2).append("<button class='uk-button uk-button-default dibs' data=" + objID[i] + ">Dibs</button>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].name + "</p>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].address + "</p>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].description + "</p>")
+  //       $(div2).append("<p>" + californiaData[objID[i]].zipcode + "</p>")
+
+  //       var user = {
+  //         "name": californiaData[objID[i]].name,
+  //         "description": californiaData[objID[i]].description,
+  //         "zipcode": californiaData[objID[i]].zipcode,
+  //         "address": californiaData[objID[i]].address
+  //       }
+  //       console.log(user)
+
+  //       userSubmissions.push(user)
+  //     }
+  //   }
+  // }
+}
+
+
+
